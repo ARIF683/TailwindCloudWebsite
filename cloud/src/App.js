48372b3f433c2c27434618a9fc490f07810f82fc
ARIF ALAM <shahazimalam683@gmail.com>
2023-05-31 14:52:58 +0000

    About Section Done.

--- cloud/src/App.js
+++ cloud/src/App.js
@@ -1,12 +1,14 @@
 
 import Navbar from './Component/Navbar';
 import Hero from "./Component/Hero";
+import About from './Component/About';
 
 function App() {
   return (
     <>
     <Navbar />
     <Hero />
+    <About />
     </>
   );
 }
---
+++ cloud/src/Component/About.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <div className = "py-[100px] flex items-center justify-center flex-col  px-5  mx-auto w-full ">
+        <div className = "text-center ">
+          <h1 className = "text-5xl font-bold ">Trusted by developers across the world</h1>
+          <p className = "text-3xl py-3 text-gray-500">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque
+            asperiores earum placeat veritatis dignissimos itaque.
+          </p>
+        </div>
+        <div className = "grid md:grid-cols-3 text-center gap-2   py-3 ">
+          <div className = "shadow-xl rounded-xl py-3 px-[50px] border ">
+            <h1 className = "text-5xl text-indigo-600 font-bold ">100%</h1>
+            <p className = "text-gray-400 ">Completion</p>
+          </div>
+          <div className = "shadow-xl rounded-xl py-3 px-[50px] border ">
+            <h1 className = "text-5xl text-indigo-600 font-bold ">24/7</h1>
+            <p className = "text-gray-400 ">Delivery</p>
+          </div>
+          <div className = "rounded-xl shadow-xl py-3 px-[50px] border  ">
+            <h1 className = "text-5xl text-indigo-600 font-bold ">100K</h1>
+            <p className = "text-gray-400 ">Transitions</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
